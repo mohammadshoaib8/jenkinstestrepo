c1aece37d4e33114f14bc00ea1873e83c2577697
mohammadshoaib <150773390+mohammadshoaib8@users.noreply.github.com>
2024-10-09 18:38:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 sudo apt update -y
 sudo apt install apache2 -y
 sudo systemctl enable apache2
-echo "hey this is shoaib from synycs" > /var/www/html/index.html
+echo "hey this is shoaib" > /var/www/html/index.html
