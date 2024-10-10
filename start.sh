0a79914ae2de9d5882e5748445734b910c0a758b
mohammadshoaib <150773390+mohammadshoaib8@users.noreply.github.com>
2024-10-10 11:59:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@
 sudo apt update -y
 sudo apt install apache2 -y
 sudo systemctl enable apache2
+sudo chown -R $USER:$USER /var/www/html
+sudo chmod -R 755 /var/www/html
 sudo echo "hello  world" > /var/www/html/index.html
